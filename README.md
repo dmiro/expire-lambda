29e2e4bb69c978190f103062af206700b5c18b65
David Miró <davidmirom@hotmail.com>
2013-06-24 10:27:24 -0400

    refactoring

--- README.md
+++ README.md
@@ -12,27 +12,28 @@ Function library to calculate the date &amp; time expiration
 
 ###### example 1
 
-    var expire = require('expire-lambda.js').expire;
-    console.log(expire.hours(3).minutes(10).inSeconds()); // 10810 seconds
-    console.log(expire.h(3).m(10).inSeconds()); // 10810 seconds
+    var expire = require('expire-lambda.js');
+    console.log(expire.in.hours(3).minutes(10).inSeconds()); // 10810 seconds
+    console.log(expire.in.h(3).m(10).inSeconds()); // 10810 seconds
 
 ###### example 2
 
-    var expire = require('expire-lambda.js').expire;
-    console.log(expire.seconds(10).getInstant().timeRemaining().inSeconds()); // 10 seconds
+    var expire = require('expire-lambda.js');
+    console.log(expire.in.seconds(10).getInstant().timeRemaining().inSeconds()); // 10 seconds
 
 ###### example 3
 
-    var miExpire = expire.seconds(10);
+    var expire = require('expire-lambda.js');
+    var miExpire = expire.in.seconds(10);
     var miInst = miExpire.getInstant();
     setTimeout(function() {  
         console.log('expired?:',miInst.isExpired()); // false
         console.log('time remaining?:',miInst.timeRemaining().inSeconds()); // 8.998
-    }, expire.seconds(1).inMilliseconds());
+    }, expire.in.seconds(1).inMilliseconds());
     setTimeout(function() {
         console.log('expired?:',miInst.isExpired());// true
         console.log('time remaining?:',miInst.timeRemaining().inSeconds()); // 0
-    }, expire.seconds(10).inMilliseconds());
+    }, expire.in.seconds(10).inMilliseconds());
 
 expire-lambda [ES]
 =============
@@ -41,28 +42,29 @@ Biblioteca de funciones para calcular la fecha y hora de caducidad
 
 ### Diagrama de actividades
 
-![expire-lambda activity diagram](https://github.com/dmiro/expire-lambda/raw/master/resource/expire_activity.png)
+![expire-lambda activity diagram](https://github.com/dmiro/expire-lambda/raw/master/resources/expire_activity.png)
 
 ###### ejemplo 1
 
-    var expire = require('expire-lambda.js').expire;
-    console.log(expire.hours(3).minutes(10).inSeconds()); // 10810 segundos
-    console.log(expire.h(3).m(10).inSeconds()); // 10810 segundos
+    var expire = require('expire-lambda.js');
+    console.log(expire.in.hours(3).minutes(10).inSeconds()); // 10810 segundos
+    console.log(expire.in.h(3).m(10).inSeconds()); // 10810 segundos
 
 ###### ejemplo 2
 
-    var expire = require('expire-lambda.js').expire;
-    console.log(expire.seconds(10).getInstant().timeRemaining().inSeconds()); // 10 segundos.
+    var expire = require('expire-lambda.js');
+    console.log(expire.in.seconds(10).getInstant().timeRemaining().inSeconds()); // 10 segundos.
 
 ###### ejemplo 3
 
-    var miExpire = expire.seconds(10);
+    var expire = require('expire-lambda.js');
+    var miExpire = expire.in.seconds(10);
     var miInst = miExpire.getInstant();
     setTimeout(function() {  
         console.log('expired?:',miInst.isExpired()); // false
         console.log('time remaining?:',miInst.timeRemaining().inSeconds()); // 8.998
-    }, expire.seconds(1).inMilliseconds());
+    }, expire.in.seconds(1).inMilliseconds());
     setTimeout(function() {
         console.log('expired?:',miInst.isExpired());// true
         console.log('time remaining?:',miInst.timeRemaining().inSeconds()); // 0
-    }, expire.seconds(10).inMilliseconds());
+    }, expire.in.seconds(10).inMilliseconds());
--- test/unit-test.js
+++ test/unit-test.js
@@ -1,47 +1,47 @@
 var assert = require('assert'),
-    expire = require('../lib/expire-lambda.js').expire;
+    expire = require('../lib/expire-lambda.js');
     
 console.log('> init test');
  
-assert.equal(expire.seconds(10).milliseconds(200).inMilliseconds(), 10*1000+200); 
+assert.equal(expire.in.seconds(10).milliseconds(200).inMilliseconds(), 10*1000+200); 
 
-assert.equal(expire.seconds(10).inSeconds(), 10);
-assert.equal(expire.s(10).inSeconds(), 10);
+assert.equal(expire.in.seconds(10).inSeconds(), 10);
+assert.equal(expire.in.s(10).inSeconds(), 10);
 
-assert.equal(expire.hours(1).minutes(10).seconds(2).inSeconds(), 60*60+10*60+2);
-assert.equal(expire.h(1).m(10).s(2).inSeconds(), 60*60+10*60+2);
+assert.equal(expire.in.hours(1).minutes(10).seconds(2).inSeconds(), 60*60+10*60+2);
+assert.equal(expire.in.h(1).m(10).s(2).inSeconds(), 60*60+10*60+2);
 
-assert.equal(expire.days(0).hours(1).minutes(0).inSeconds(), 60*60);
-assert.equal(expire.days(0).h(1).m(0).inSeconds(), 60*60);
+assert.equal(expire.in.days(0).hours(1).minutes(0).inSeconds(), 60*60);
+assert.equal(expire.in.days(0).h(1).m(0).inSeconds(), 60*60);
 
-assert.equal(expire.days(1).minutes(10).inSeconds(), 24*60*60+10*60);  
-assert.equal(expire.d(1).m(10).inSeconds(), 24*60*60+10*60);  
+assert.equal(expire.in.days(1).minutes(10).inSeconds(), 24*60*60+10*60);  
+assert.equal(expire.in.d(1).m(10).inSeconds(), 24*60*60+10*60);  
 
-assert.equal(expire.days(0).hours(3).inSeconds(), 3*60*60);
-assert.equal(expire.d(0).h(3).inSeconds(), 3*60*60);
+assert.equal(expire.in.days(0).hours(3).inSeconds(), 3*60*60);
+assert.equal(expire.in.d(0).h(3).inSeconds(), 3*60*60);
 
-assert.equal(expire.hours(3).inSeconds(), 3*60*60);
-assert.equal(expire.hours(3).inMinutes(), 3*60);
-assert.equal(expire.hours(3).inHours(), 3);
-assert.equal(expire.hours(3).inDays(), 3/24);
-assert.equal(expire.h(3).inSeconds(), 3*60*60);
-assert.equal(expire.h(3).inMinutes(), 3*60);
-assert.equal(expire.h(3).inHours(), 3);
-assert.equal(expire.h(3).inDays(), 3/24);
+assert.equal(expire.in.hours(3).inSeconds(), 3*60*60);
+assert.equal(expire.in.hours(3).inMinutes(), 3*60);
+assert.equal(expire.in.hours(3).inHours(), 3);
+assert.equal(expire.in.hours(3).inDays(), 3/24);
+assert.equal(expire.in.h(3).inSeconds(), 3*60*60);
+assert.equal(expire.in.h(3).inMinutes(), 3*60);
+assert.equal(expire.in.h(3).inHours(), 3);
+assert.equal(expire.in.h(3).inDays(), 3/24);
 
-assert.throws(function(){expire.minuts(5).days(1).miSeconds()}, Error);
-assert.throws(function(){expire.minuts(5).s(8).days(1).miSeconds()}, Error);
+assert.throws(function(){expire.in.minuts(5).days(1).miSeconds()}, Error);
+assert.throws(function(){expire.in.minuts(5).s(8).days(1).miSeconds()}, Error);
 
-assert.equal(expire.seconds(10).getInstant().isExpired(), false);
-assert.equal(expire.seconds(10).getInstant().timeRemaining().inSeconds(), 10);
-assert.equal(expire.d(20).h(0).m(30).s(10).getInstant(new Date("December 17, 1995 03:24:00")).timeRemaining().inSeconds(), 0);
-assert.notEqual(expire.d(20).h(0).m(30).s(10).getInstant(new Date("December 31, 2100 10:00:00")).timeRemaining().inSeconds(), 0);
-assert.equal(expire.seconds(10).getInstant(new Date("December 17, 1995 03:24:00")).isExpired(), true);
-assert.equal(expire.seconds(10).getInstant(new Date("December 31, 2100 10:00:00")).isExpired(), false);
+assert.equal(expire.in.seconds(10).getInstant().isExpired(), false);
+assert.equal(expire.in.seconds(10).getInstant().timeRemaining().inSeconds(), 10);
+assert.equal(expire.in.d(20).h(0).m(30).s(10).getInstant(new Date("December 17, 1995 03:24:00")).timeRemaining().inSeconds(), 0);
+assert.notEqual(expire.in.d(20).h(0).m(30).s(10).getInstant(new Date("December 31, 2100 10:00:00")).timeRemaining().inSeconds(), 0);
+assert.equal(expire.in.seconds(10).getInstant(new Date("December 17, 1995 03:24:00")).isExpired(), true);
+assert.equal(expire.in.seconds(10).getInstant(new Date("December 31, 2100 10:00:00")).isExpired(), false);
 
-var miInst =expire.seconds(1).getInstant();
-setTimeout(function() {assert.equal(miInst.isExpired(),true);}, expire.seconds(2).inMilliseconds());
-var miInst =expire.seconds(2).getInstant();
-setTimeout(function() {assert.equal(miInst.isExpired(),false);}, expire.seconds(1).inMilliseconds());
+var miInst =expire.in.seconds(1).getInstant();
+setTimeout(function() {assert.equal(miInst.isExpired(),true);}, expire.in.seconds(3).inMilliseconds());
+var miInst =expire.in.seconds(3).getInstant();
+setTimeout(function() {assert.equal(miInst.isExpired(),false);}, expire.in.seconds(1).inMilliseconds());
 
 console.log('> end test');
